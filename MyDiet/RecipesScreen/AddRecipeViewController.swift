78ef3fed4122f2ea0d17510e15d15b6825df2320
Chiglintsev Artem <chiglincev.artem@yandex.ru>
2020-01-31 13:10:25 +0600

    start make transition

---
+++ MyDiet/RecipesScreen/AddRecipeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddRecipeViewController.swift
+//  MyDiet
+//
+//  Created by Artas on 29/01/2020.
+//  Copyright © 2020 Артем Чиглинцев. All rights reserved.
+//
+
+import UIKit
+
+class AddRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
